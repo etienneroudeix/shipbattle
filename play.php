a7da98b41c63753818a395cbe8825655caea2587
etienne <er@4x.fr>
2018-10-18 20:54:03 +0200

    Wow ! we already get the IP !

---
+++ play.php
@@ -0,0 +1,24 @@
+<?php
+
+use EventLoop\EventLoop;
+use Rx\Scheduler\EventLoopScheduler;
+
+require __DIR__ . "/vendor/autoload.php";
+
+$loop = EventLoop::getLoop();
+$scheduler = new EventLoopScheduler($loop);
+
+$ip = getIp();
+
+
+
+
+function getIp()
+{
+    GET_IP:
+    $ip = readline("Please enter other player IP address : ");
+    if (!filter_var($ip, FILTER_VALIDATE_IP)) goto GET_IP;
+
+    return $ip;
+}
+
